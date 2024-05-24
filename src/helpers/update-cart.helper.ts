7d31dc278a6f2464c3a15141c2882be1215ee0e6
owl352 <kokosinca123@gmail.com>
2024-05-25 01:31:00 +0500

    cart code refactor

---
+++ src/helpers/update-cart.helper.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { cartModel } from "../models";
+
+export async function updateCart(
+  userCart: any,
+  id: string,
+  count: string
+): Promise<boolean> {
+  let haveThis: boolean = false;
+  let newCart = userCart.items;
+  for (let el of userCart.items) {
+    if (el.id.toString() == id) {
+      haveThis = true;
+      if (count == "0") {
+        newCart = newCart.filter(
+          (_) => _ != el
+        ) as mongoose.Types.DocumentArray<{
+          id: mongoose.Types.ObjectId;
+          count: number;
+        }>;
+      } else {
+        newCart[newCart.findIndex((_) => _ == el)].count = parseInt(count);
+      }
+      break;
+    }
+  }
+  if (!haveThis) {
+    if (count != 0) {
+      newCart.push({
+        id: mongoose.Types.ObjectId.createFromHexString(id),
+        count: count,
+      });
+    } else {
+      return false;
+    }
+  }
+  await cartModel.findByIdAndUpdate(userCart._id, { items: newCart });
+  return true;
+}
